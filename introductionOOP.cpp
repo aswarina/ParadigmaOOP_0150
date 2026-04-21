fb49d940ea8a1e57043c09a822374bc0825ad48e
aswarina <lyxsyyawa@gmail.com>
2026-04-21 17:56:59 +0700

    membuat header fungsi printData

--- introductionOOP.cpp
+++ introductionOOP.cpp
@@ -6,4 +6,10 @@ public :
     int nim;
     string nama;
     float nilai;
+
+    void printData(){
+        cout << "NIM : " << nim << endl;
+        cout << "NAMA : " << nama << endl;
+        cout << "NILAI : " << nilai << endl;
+    }
 }
